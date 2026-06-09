0e2e4ea1a41922fac9644687bd78ac2f641f6c65
Mariliis Metsjärv <mariliis.metsjarv@vikk.ee>
2026-06-09 13:26:07 +0300

    Update Ping pong 2.py

--- Ping pong 2.py
+++ Ping pong 2.py
@@ -1,25 +1,37 @@
 import pygame
 import sys
 
-pygame.init() # Pygame'i käivitamine
-pygame.mixer.init() # Helimooduli käivitamine
+pygame.init()  # Pygame'i käivitamine
+pygame.mixer.init()  # Helimooduli käivitamine
+
 
 # Ekraani mõõtmed
-SCREEN_WIDTH = 640
-SCREEN_HEIGHT = 480
+screen_width = 640
+screen_height = 480
 
 # Värvide defineerimine
-TAUST = (153, 232, 158)      # Heleroheline taust
-SCORE_COLOR = (0, 102, 51)   # Tume roheline skoor
+taust = (153, 232, 158)         # Heleroheline taust
+roheline = (0, 102, 51)    # Tekstid
+
 
 # Mänguakna loomine
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Ping pong - Metsjärv")
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Ping pong 2 - Metsjärv")
 
-clock = pygame.time.Clock() # Kell FPS-i kontrollimiseks
-font = pygame.font.SysFont("comicsansms", 24, bold=True) # Fondi loomine skoori kuvamiseks
-pygame.mixer.music.load("dino_2026.mp3") # Taustamuusika laadimine
-pygame.mixer.music.play(-1) # Muusika mängimine lõputus tsüklis (-1)
+clock = pygame.time.Clock()  # Kell FPS-i kontrollimiseks
+font = pygame.font.SysFont("comicsansms", 24, bold=True)  # Fondi loomine skoori kuvamiseks
+pygame.mixer.music.load("Raining tacos.mp3")
+
+# Helitugevus vahemikus 0.0 kuni 1.0
+pygame.mixer.music.set_volume(0.1)  # 30% tugevus
+
+pygame.mixer.music.play(-1)
+# Muud heliefektid
+bounce_sound = pygame.mixer.Sound("Bounce.mp3")
+gameover_sound = pygame.mixer.Sound("Surm.mp3")
+
+bounce_sound.set_volume(1.0)
+gameover_sound.set_volume(0.8)
 
 # Palli pildi laadimine ja suuruse muutmine
 ball_img = pygame.image.load("ball.png")
@@ -30,8 +42,8 @@ paddle_img = pygame.image.load("pad.png")
 paddle_img = pygame.transform.scale(paddle_img, (120, 20))
 
 # Palli algasukoht
-ball_x = SCREEN_WIDTH // 2
-ball_y = SCREEN_HEIGHT // 4
+ball_x = screen_width // 2
+ball_y = screen_height // 4
 
 # Palli liikumiskiirus
 ball_speed_x = 4
@@ -41,16 +53,16 @@ ball_speed_y = 4
 paddle_width = 120
 paddle_height = 20
 
-paddle_x = (SCREEN_WIDTH - paddle_width) // 2 # Asetab aluse ekraani keskele
-paddle_y = int(SCREEN_HEIGHT / 1.1) # Asetab aluse ekraani alumisse ossa
-paddle_speed = 8 # Aluse liikumiskiirus
-score = 0 # Mängija algskoor
+paddle_x = (screen_width - paddle_width) // 2  # Asetab aluse ekraani keskele
+paddle_y = int(screen_height / 1.1)  # Asetab aluse ekraani alumisse ossa
+paddle_speed = 8  # Aluse liikumiskiirus
+score = 0  # Mängija algskoor
 
 # Mängu põhitsükkel
 running = True
 while running:
 
-    for event in pygame.event.get(): # Sündmuste kontrollimine
+    for event in pygame.event.get():  # Sündmuste kontrollimine
 
         # Kui kasutaja sulgeb akna
         if event.type == pygame.QUIT:
@@ -63,7 +75,7 @@ while running:
         paddle_x -= paddle_speed
 
     # Parem nool
-    if keys[pygame.K_RIGHT] and paddle_x < SCREEN_WIDTH - paddle_width:
+    if keys[pygame.K_RIGHT] and paddle_x < screen_width - paddle_width:
         paddle_x += paddle_speed
 
     # Palli liikumine
@@ -71,18 +83,99 @@ while running:
     ball_y += ball_speed_y
 
     # Vasaku või parema seina põrge
-    if ball_x <= 0 or ball_x + 20 >= SCREEN_WIDTH:
+    if ball_x <= 0 or ball_x + 20 >= screen_width:
         ball_speed_x *= -1
 
     # Ülemine sein
     if ball_y <= 0:
         ball_speed_y *= -1
 
-    # Kui pall puudutab ekraani alumist serva
-    if ball_y + 20 >= SCREEN_HEIGHT:
-        print(f"Mäng läbi! Sinu skoor: {score}")
-        running = False
+    if ball_y + 20 >= screen_height:
+        gameover_sound.play()
 
+        game_over = True
+
+        while game_over:
+            for event in pygame.event.get():
+
+                if event.type == pygame.QUIT:
+                    game_over = False
+                    running = False
+
+                if event.type == pygame.KEYDOWN:
+
+                    # Q = välju
+                    if event.key == pygame.K_q:
+                        game_over = False
+                        running = False
+
+                    # C = jätka mängu
+                    if event.key == pygame.K_r:
+                        # Lähtesta pall
+                        ball_x = screen_width // 2
+                        ball_y = screen_height // 4
+
+                        ball_speed_x = 4
+                        ball_speed_y = 4
+
+                        # Lähtesta alus
+                        paddle_x = (screen_width - paddle_width) // 2
+
+                        score = 0
+                        game_over = False
+
+            screen.fill(taust)
+
+            gameover_font = pygame.font.SysFont("comicsansms", 30, bold=True)
+            score_font = pygame.font.SysFont("comicsansms", 24, bold=True)
+
+            gameover_text = gameover_font.render(
+                "Mäng läbi!",
+                True,
+                roheline
+            )
+
+            score_text = score_font.render(
+                f"Sinu skoor: {score}",
+                True,
+                roheline
+            )
+
+            continue_text = score_font.render(
+                "R - Proovi uuesti",
+                True,
+                roheline
+            )
+
+            quit_text = score_font.render(
+                "Q - Quit",
+                True,
+                roheline
+            )
+
+            screen.blit(
+                gameover_text,
+                (screen_width // 2 - gameover_text.get_width() // 2, 140)
+            )
+
+            screen.blit(
+                score_text,
+                (screen_width // 2 - score_text.get_width() // 2, 200)
+            )
+
+            screen.blit(
+                continue_text,
+                (screen_width // 2 - continue_text.get_width() // 2, 260)
+            )
+
+            screen.blit(
+                quit_text,
+                (screen_width // 2 - quit_text.get_width() // 2, 300)
+            )
+
+            pygame.display.flip()
+
+        continue
     # Loome ristkülikud kokkupõrgete kontrollimiseks
     ball_rect = pygame.Rect(ball_x, ball_y, 20, 20)
     paddle_rect = pygame.Rect(
@@ -91,28 +184,30 @@ while running:
         paddle_width,
         paddle_height)
 
-    if ball_rect.colliderect(paddle_rect): # Kontrollib, kas pall puudutab alust
-        if ball_speed_y > 0: # Ainult siis, kui pall liigub alla
-            ball_speed_y *= -1 # Muudab vertikaalse liikumissuuna vastupidiseks
-            score += 1 # Suurendab skoori
+    if ball_rect.colliderect(paddle_rect):
+        if ball_speed_y > 0:
+            ball_speed_y *= -1
+            score += 1
 
-            # Muudab palli iga põrkega veidi kiiremaks
+            # Mängib põrkeheli
+            bounce_sound.play()
+
             ball_speed_x *= 1.05
             ball_speed_y *= 1.05
 
-    screen.fill(TAUST) # Täidab tausta värviga
-    screen.blit(ball_img, (ball_x, ball_y)) # Joonistab palli
-    screen.blit(paddle_img, (paddle_x, paddle_y)) # Joonistab aluse
+    screen.fill(taust)  # Täidab tausta värviga
+    screen.blit(ball_img, (ball_x, ball_y))  # Joonistab palli
+    screen.blit(paddle_img, (paddle_x, paddle_y))  # Joonistab aluse
 
     # Kuvab skoori ekraanile
     score_text = font.render(
         f"Skoor: {score}",
         True,
-        SCORE_COLOR)
-        
-    screen.blit(score_text, (20, 20)) # Skoori asukoht
-    pygame.display.flip() # Uuendab ekraani
-    clock.tick(60) # Piirab mängu kiiruse 60 kaadrini sekundis
+        roheline)
+
+    screen.blit(score_text, (20, 20))  # Skoori asukoht
+    pygame.display.flip()  # Uuendab ekraani
+    clock.tick(60)  # Piirab mängu kiiruse 60 kaadrini sekundis
 
 # Mängu sulgemine
 pygame.quit()
